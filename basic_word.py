d7acf65d73254ea1455f96057b54f760cf75c223
aperushin <aperushin@gmail.com>
2022-06-19 20:56:40 +0400

    Revert "Add Player class"
    This reverts commit 1461a5f5e029fdd7ffe44f224809da694f355dd3.

--- basic_word.py
+++ basic_word.py
@@ -1,21 +1,23 @@
 class BasicWord:
 
-    def __init__(self, word: str, subwords: list | set) -> None:
+    def __init__(self, word: str, subwords: list):
         self.word = word
         self.subwords = set(subwords)
 
-    def __repr__(self) -> str:
-        return f'{self.__class__.__name__}({self.word!r}, {self.subwords!r})'
+    def __repr__(self):
+        return f"'{self.word}'.BasicWord"
 
     def is_subword(self, subword: str) -> bool:
         """
         Проверяет наличие подслова в списке подслов
-        :param str subword: Подслово для проверки
+        :param str subword:
         :return bool: True если найдено, False если нет
         """
         return subword in self.subwords
 
-    @property
-    def subwords_count(self) -> int:
-        """Возвращает количество подслов"""
+    def get_subwords_quantity(self) -> int:
+        """
+        Возвращает количество подслов
+        :return int:
+        """
         return len(self.subwords)
