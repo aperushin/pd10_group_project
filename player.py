393a58f9cb79bac71d9461462bc2adeab6a6aa6e
evgenyglushko <107578966+evgenyglushko@users.noreply.github.com>
2022-06-19 19:30:48 +0500

    Update player.py

--- player.py
+++ player.py
@@ -1,2 +1,36 @@
 class Player:
-    pass
+
+    def __init__(self, name):
+        self.name = name
+        self.used_words = set()
+
+    def count_used_word(self):
+        """получение количества использованных
+        слов (возвращает int);
+        :param
+        :return int() """
+
+        return int(len(self.used_words))
+
+    def add_used_word(self, word):
+        """добавление слова в использованные
+        слова (ничего не возвращает);
+        :param
+        :return None"""
+
+        self.word = word
+        self.used_words.append(self.word)
+
+    def check_used_word(self, word):
+        """проверка использования данного
+        слова до этого (возвращает bool).
+        :param
+        :return bool """
+
+        return word in self.used_words
+
+    def __repr__(self):
+        return f'Player {self.name}'
+
+    def __str__(self):
+        return self.name
