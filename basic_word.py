1461a5f5e029fdd7ffe44f224809da694f355dd3
aperushin <aperushin@gmail.com>
2022-06-19 20:48:10 +0400

    Add Player class

--- basic_word.py
+++ basic_word.py
@@ -1,23 +1,21 @@
 class BasicWord:
 
-    def __init__(self, word: str, subwords: list):
+    def __init__(self, word: str, subwords: list | set) -> None:
         self.word = word
         self.subwords = set(subwords)
 
-    def __repr__(self):
-        return f"'{self.word}'.BasicWord"
+    def __repr__(self) -> str:
+        return f'{self.__class__.__name__}({self.word!r}, {self.subwords!r})'
 
     def is_subword(self, subword: str) -> bool:
         """
         Проверяет наличие подслова в списке подслов
-        :param str subword:
+        :param str subword: Подслово для проверки
         :return bool: True если найдено, False если нет
         """
         return subword in self.subwords
 
-    def get_subwords_quantity(self) -> int:
-        """
-        Возвращает количество подслов
-        :return int:
-        """
+    @property
+    def subwords_count(self) -> int:
+        """Возвращает количество подслов"""
         return len(self.subwords)
