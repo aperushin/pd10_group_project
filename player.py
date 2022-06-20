fa087111d46b8d8468bd62eb7d18d50d209909d5
aperushin <aperushin@gmail.com>
2022-06-20 19:26:15 +0400

    Tweaks and minor improvements

--- player.py
+++ player.py
@@ -1,9 +1,12 @@
+from dataclasses import dataclass, field
+
+
+@dataclass
 class Player:
 
-    def __init__(self, name: str) -> None:
-        self.name = name
-        self.used_words = set()
-        self.correct_answers = 0
+    name: str
+    correct_answers: int = 0
+    used_words: set[str] = field(init=False, default_factory=set)
 
     def count_used_words(self) -> int:
         """Считает количество использованных слов"""
@@ -16,9 +19,6 @@ class Player:
     def has_used_word(self, word: str) -> bool:
         """Проверяет наличие слова в списке использованных слов"""
         return word in self.used_words
-
-    def __repr__(self) -> str:
-        return f'{self.__class__.__name__}({self.name!r})'
 
     def __str__(self) -> str:
         return f'Игрок {self.name}'
